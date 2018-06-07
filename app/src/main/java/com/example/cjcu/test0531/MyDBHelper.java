136f5291191472ec648a8b6707536d1789d9c932
jiaro123 <g24561099@mailst.cjcu.edu.tw>
2018-06-07 16:06:35 +0800

    finish

--- app/src/main/java/com/example/cjcu/test0531/MyDBHelper.java
+++ app/src/main/java/com/example/cjcu/test0531/MyDBHelper.java
@@ -9,7 +9,16 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class MyDBHelper extends SQLiteOpenHelper {
-    public MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    private static MyDBHelper instance;
+
+    public static MyDBHelper getInstance(Context c){
+        if(instance == null){
+            instance=new MyDBHelper(c,"my_db",null,1);
+        }
+        return instance;
+    }
+
+    private MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
